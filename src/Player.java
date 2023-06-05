aca93bbb4a7be3f57f7b4d5c679938c5a8066680
elaine <elainez13@nycstudents.net>
2023-06-05 13:00:38 -0400

    draft

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,10 @@
 public class Player {
     private String icon;
+    private boolean win;
 
     public Player() {
         icon = null;
+        win = false;
     }
 
     public void setIcon(String i) {
@@ -11,5 +13,13 @@ public class Player {
 
     public String getIcon() {
         return icon;
+    }
+
+    public void setWin(boolean i) {
+        win = i;
+    }
+
+    public boolean getWin() {
+        return win;
     }
 }
